e73b110a5c0f1c1260e4de190951388c31207a3a
Paduraru C <lambarjack@pm.me>
2022-01-17 15:33:46 +0200

    Added DB, HTML Pages, and stuff

--- DataBase/database.py
+++ DataBase/database.py
@@ -1,1 +1,17 @@
-import flask
+import sqlite3
+
+conn = sqlite3.connect('DataBaseWeb.db')
+
+# cursor
+c = conn.cursor()
+
+c.execute("SELECT rowid, * FROM customers")
+
+items = c.fetchall()
+
+for item in items:
+    print(item)
+
+conn.commit()
+
+conn.close
