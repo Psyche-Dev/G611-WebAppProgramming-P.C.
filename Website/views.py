c6614e8ea3826fc812f8fd5ac3758f3cf58a94c5
Paduraru C <lambarjack@pm.me>
2022-01-22 14:40:30 +0200

    Added forum reply.

--- Website/views.py
+++ Website/views.py
@@ -1,13 +1,27 @@
 from flask_login import login_required,current_user
-from flask import Blueprint, render_template
+from flask import Blueprint, flash, render_template, request
+from .models import Forumpost
+from . import db
 
 views = Blueprint('views', __name__)
 
 @views.route('/', methods=['GET', 'POST'])
 @login_required
 def forum():
+    if request.method == 'POST':
+        post = request.form.get('forumpost')
+
+        if len(post) < 5:
+            flash('Forum post must be greater than 5 chanracters', category='error')
+        else:
+            newreply = Forumpost(data=post, user_id = current_user.id)
+            db.session.add(newreply)
+            db.session.commit()
+            flash('Reply created', category='success')
+
     return render_template("forum.html")
 
 @views.route('/news')
 def news():
     return render_template("news.html")
+
